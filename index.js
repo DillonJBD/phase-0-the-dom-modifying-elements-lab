56444baef4bb998ac1242416fac5642e7d73fb89
DillonBorden <dillondaughtry2012@gmail.com>
2024-01-07 20:39:49 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const h1 = document.createElement("h1");
+
+const newHeader = h1;
+newHeader.id = "victory";
+newHeader.textContent = "Dillon is the champion";
